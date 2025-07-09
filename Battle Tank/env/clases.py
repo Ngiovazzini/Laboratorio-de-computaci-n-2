88b7d916af99cb346904e9c89a433dfda9df05b9
Nicolas <ngiovazzini@gmail.com>
2025-07-09 20:06:23 -0300

    Def-Update

--- Battle Tank/env/clases.py
+++ Battle Tank/env/clases.py
@@ -8,4 +8,15 @@ class Tank(py.sprite.Sprite):
         image_s_escalar = pygame.image.load(ruta).convert()
         self.image = py.transform.scale(image_s_escalar,(tile_size,tile_size) )
         self.rect = self.image.get_rect()
-        self.rect.center=(x,y)
+        self.rect.topleft = (x, y)
+
+    def update(self):
+        teclas=py.key.get_pressed() #esto devuelve un true si una tecla es presionada
+        if teclas[py.K_UP]:
+            self.rect.move_ip(0,-2)
+        if teclas[py.K_DOWN]:
+            self.rect.move_ip(0,2)
+        if teclas[py.K_LEFT]:
+            self.rect.move_ip(-2,0)
+        if teclas[py.K_RIGHT]:
+            self.rect.move_ip(2,0)
